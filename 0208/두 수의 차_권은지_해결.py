173bbd4e476e073769d9bb2ecbc4863c3139341d
Sienna <130072475+siennavely@users.noreply.github.com>
2024-02-06 12:59:03 +0900

    Create 두 수의 차_권은지_해결.py

---
+++ 0208/두 수의 차_권은지_해결.py
@@ -0,0 +1,12 @@
+#def solution(num1, num2):
+ #   answer = 0
+  #  return answer
+
+#def solution(num1, num2):
+ #   if -50000 <= num1 <= 50000 and -50000 <= num2 <= 50000:
+  #      return num1 - num2
+   # else:
+    #    return "입력값이 제한사항을 벗어납니다."
+
+def solution (num1, num2):
+    return num1 - num2
