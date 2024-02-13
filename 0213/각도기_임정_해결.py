15468ca35b728113a478b63a6f1e6684c906ee4b
Lim Jung <39439424+bellepoque7@users.noreply.github.com>
2024-02-13 10:59:07 +0900

    Create 각도기_임정_해결.py

---
+++ 0213/각도기_임정_해결.py
@@ -0,0 +1,14 @@
+def solution(angle):
+    
+    if angle > 0 and angle < 90:
+        answer = 1
+    elif angle == 90:
+        answer = 2
+    elif angle > 90 and angle < 180:
+        answer = 3
+    #위 조건이 해당하지 않으면 180도 이다. 
+    else:
+        answer = 4
+    # elif angle == 180: 
+    #     answer = 4
+    return answer
