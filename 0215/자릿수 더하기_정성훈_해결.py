7f290bc1e1696c3e5c9c41a0873c16ac9c382f92
JeongSeongHun <gnsl1465@naver.com>
2024-02-15 10:22:56 +0900

    Update 자릿수 더하기_정성훈_해결.py

--- 0215/자릿수 더하기_정성훈_해결.py
+++ 0215/자릿수 더하기_정성훈_해결.py
@@ -1,3 +1,4 @@
+# 내 정답 풀이
 def solution(n):
     answer = 0
     list_n = list(str(n))
@@ -5,4 +6,19 @@ def solution(n):
     for i in list_n:
         answer+=int(i)
 
+    return answer
+
+# 튜터님 정답 풀이
+def solution(n):
+    answer = 0
+    
+    while True:
+        answer = answer + n%10
+        n = n//10
+        
+#        한자리 수 일 경우 계산
+        if n // 10 == 0:
+            answer = answer + n
+            break
+      
     return answer
