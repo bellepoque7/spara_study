5fda20dfc9068ccf233671b05a6a7475c10167cd
DIB-PP <155515440+DIB-PP@users.noreply.github.com>
2024-02-22 03:11:24 +0900

    Create 정수 내림차순으로 배치하기_황기쁨_미해결.py

---
+++ 0222/정수 내림차순으로 배치하기_황기쁨_미해결.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = 0
+    return int(answer.reverse())
+
+# reverse를 사용하면 뒤집기가 된다고 생각했는데 int라서 계속 안된다고 함.
+# return int(str(answer).reverse())도 해보고
+# return int(answer).revers()도 해보고 
+# return int(answer.reverse())도 해봤지만 다 int라서 안된다고 했음. 
