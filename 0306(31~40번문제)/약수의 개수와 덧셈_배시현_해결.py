42f21515151dec53d3b9abc7df28b740e1674581
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-03-02 01:23:09 +0900

    Update 약수의 개수와 덧셈_배시현_해결.py

--- 0306(31~40번문제)/약수의 개수와 덧셈_배시현_해결.py
+++ 0306(31~40번문제)/약수의 개수와 덧셈_배시현_해결.py
@@ -1,7 +1,7 @@
 def solution(left, right):
     result = []
     answer = 0
-    for i in range(left, right+1):
+    for i in range(left, right+1): # 14번 응용
         for x in range(1, i+1):
             if i % x == 0:
                 result.append(x)
