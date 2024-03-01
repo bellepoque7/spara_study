30a9b908ca02b2df61e1ccfeece7353fdbb35429
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-03-02 01:22:12 +0900

    Create 약수의 개수와 덧셈_배시현_해결.py

---
+++ 0306(31~40번문제)/약수의 개수와 덧셈_배시현_해결.py
@@ -0,0 +1,14 @@
+def solution(left, right):
+    result = []
+    answer = 0
+    for i in range(left, right+1):
+        for x in range(1, i+1):
+            if i % x == 0:
+                result.append(x)
+        if len(result) % 2 == 0:
+            answer+=i
+            result = []
+        else:
+            answer-=i
+            result = []
+    return answer
