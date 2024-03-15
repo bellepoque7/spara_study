b2d5efcb6b5f53fd501d4384b27b8216c75132e5
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-03-15 15:05:57 +0900

    Update 3진법 뒤집기_배시현_시간초과(해결).py

--- 0306(31~40번문제)/3진법 뒤집기_배시현_시간초과(해결).py
+++ 0306(31~40번문제)/3진법 뒤집기_배시현_시간초과(해결).py
@@ -11,3 +11,23 @@ def solution(n):
     for i in range(len(result)):
         answer += result[i]*3**i
     return answer
+
+
+
+
+# 이전에 틀린 코드
+
+def solution(n):
+    result = []
+    while divmod(n, 3):
+        result.append(divmod(n,3)[1])
+        n = divmod(n,3)[0]
+        if n == 1:
+            result.append(1) # 몫이 1이 될때까지 나눈다 라는 생각이 맞지 않았음. ex) 1,2 등 몫이 0일 때가 무한루프.
+            result.reverse()
+            break
+    answer = 0
+    for i in range(len(result)):
+        answer += result[i]*3**i
+    return answer
+
