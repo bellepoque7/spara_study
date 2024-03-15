ba708f294452a735f070526716984f1222611285
Si Hyeon Bae <156559007+dataosean@users.noreply.github.com>
2024-03-15 15:04:24 +0900

    Update and rename 3진법 뒤집기_배시현_시간초과.py to 3진법 뒤집기_배시현_시간초과(해결).py

--- 0306(31~40번문제)/3진법 뒤집기_배시현_시간초과(해결).py
+++ 0306(31~40번문제)/3진법 뒤집기_배시현_시간초과(해결).py
@@ -3,8 +3,8 @@ def solution(n):
     while divmod(n, 3):
         result.append(divmod(n,3)[1])
         n = divmod(n,3)[0]
-        if n == 1:
-            result.append(1)
+        if n == 0: # 삼진법을 아예 몫이 0이 될 때까지 해야했다.
+            # result.append(1) 몫이 1이면 1을 넣는다는 한 자릿수일 때 해당되지 않음.
             result.reverse()
             break
     answer = 0
